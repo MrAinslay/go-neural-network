3248748a23f4533b97d48703c8007439a28a8019
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-29 19:27:55 +0200

    Update neural-net.go

--- neural-net.go
+++ neural-net.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type neuralNetConfig struct {
 	inputNeurons  int
 	outputNeurons int
@@ -18,4 +23,48 @@ type neuralNet struct {
 
 func newNetwork(config neuralNetConfig) *neuralNet {
 	return &neuralNet{config: config}
+}
+
+func (nn *neuralNet) train(x, y *mat.Dense) error {
+
+	// Initialize biases/weights.
+	randSource := rand.NewSource(time.Now().UnixNano())
+	randGen := rand.New(randSource)
+
+	wHidden := mat.NewDense(nn.config.inputNeurons, nn.config.hiddenNeurons, nil)
+	bHidden := mat.NewDense(1, nn.config.hiddenNeurons, nil)
+	wOut := mat.NewDense(nn.config.hiddenNeurons, nn.config.outputNeurons, nil)
+	bOut := mat.NewDense(1, nn.config.outputNeurons, nil)
+
+	wHiddenRaw := wHidden.RawMatrix().Data
+	bHiddenRaw := bHidden.RawMatrix().Data
+	wOutRaw := wOut.RawMatrix().Data
+	bOutRaw := bOut.RawMatrix().Data
+
+	for _, param := range [][]float64{
+		wHiddenRaw,
+		bHiddenRaw,
+		wOutRaw,
+		bOutRaw,
+	} {
+		for i := range param {
+			param[i] = randGen.Float64()
+		}
+	}
+
+	// Define the output of the neural network.
+	output := new(mat.Dense)
+
+	// Use backpropagation to adjust the weights and biases.
+	if err := nn.backpropagate(x, y, wHidden, bHidden, wOut, bOut, output); err != nil {
+		return err
+	}
+
+	// Define our trained neural network.
+	nn.wHidden = wHidden
+	nn.bHidden = bHidden
+	nn.wOut = wOut
+	nn.bOut = bOut
+
+	return nil
 }
