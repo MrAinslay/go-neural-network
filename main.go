1bba2f19e5823b44e8fb2f18e094d3703e90b96a
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-31 12:29:38 +0200

    gonum dot helper function

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "gonum.org/v1/gonum/mat"
+
 func main() {
 
+}
+
+func dot(m, n mat.Matrix) mat.Matrix {
+	r, _ := m.Dims()
+	_, c := n.Dims()
+	o := mat.NewDense(r, c, nil)
+	o.Product(m, n)
+	return o
 }
