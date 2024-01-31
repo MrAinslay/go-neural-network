1659730ac7173a60d9c5766505e048774fcc235d
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-31 14:08:28 +0200

    create network func

--- network.go
+++ network.go
@@ -18,4 +18,7 @@ func CreateNetwork(input, hidden, output int, rate float64) (net Network) {
 		outputs:      output,
 		learningRate: rate,
 	}
+	net.hiddenWeights = mat.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
+	net.outputWeights = mat.NewDense(net.outputs, net.hiddens, randomArray(net.hiddens*net.outputs, float64(net.hiddens)))
+	return
 }
