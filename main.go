b959e1b1f009e605d3ad160e0b9c3f42d6ff466f
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-31 13:56:46 +0200

    Network type

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "gonum.org/v1/gonum/mat"
 
+type Network struct {
+	inputs        int
+	hiddens       int
+	outputs       int
+	hiddenWeights *mat.Dense
+	outputWeights *mat.Dense
+	learningRate  float64
+}
+
 func main() {
 
 }
