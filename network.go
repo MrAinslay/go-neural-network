4235afeb9b1e03f46e430af2c059aeeb69d42de2
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-31 14:17:10 +0200

    predict method and sigmoid func

--- network.go
+++ network.go
@@ -1,6 +1,10 @@
 package main
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"math"
+
+	"gonum.org/v1/gonum/mat"
+)
 
 type Network struct {
 	inputs        int
@@ -21,4 +25,18 @@ func CreateNetwork(input, hidden, output int, rate float64) (net Network) {
 	net.hiddenWeights = mat.NewDense(net.hiddens, net.inputs, randomArray(net.inputs*net.hiddens, float64(net.inputs)))
 	net.outputWeights = mat.NewDense(net.outputs, net.hiddens, randomArray(net.hiddens*net.outputs, float64(net.hiddens)))
 	return
+}
+
+func (net Network) Predict(inputData []float64) mat.Matrix {
+	//forward propagination
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+	return finalOutputs
+}
+
+func sigmoid(r, c int, z float64) float64 {
+	return 1.0 / (1 + math.Exp(-1*z))
 }
